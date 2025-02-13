e436653cb6a006402ad113962fff7e33ead683a6
sanelvis <sanelvis88@gmail.com>
2025-02-12 20:27:18 -0800

    Update server.py

--- executables/server.py
+++ executables/server.py
@@ -3,7 +3,6 @@ import asyncio
 
 PORT = 7890
 
-print("testing on port " + str(PORT))
 connected_clients = set()
 
 async def messaging(websocket): 
@@ -26,8 +25,8 @@ async def messaging(websocket):
         
 async def main():
     try:
-        server = await websockets.serve(messaging, "127.0.0.1", PORT)
-        print(f"WebSocket server started on ws://127.0.0.1:{PORT}")
+        server = await websockets.serve(messaging, "0.0.0.0", PORT)
+        print(f"Websocket server started on ws://0.0.0.0:{PORT}")
         await server.wait_closed()
     except Exception as e:
         print(f"Fatal server error: {e}")
